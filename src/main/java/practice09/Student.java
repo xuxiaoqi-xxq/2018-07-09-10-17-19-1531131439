72154653c3ed2453ce59d827aed3d1b274e001a7
xxq <xxq_work@163.com>
2020-07-22 21:14:19 +0800

    [eva xu][bug fix] Add student into class

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -7,6 +7,7 @@ public class Student extends Person {
     public Student(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
+        this.klass.appendMember(this);
     }
 
     public Klass getKlass() {
