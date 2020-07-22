bb626ed167274baf20ea2fbcb624405c0ca634ca
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-22 16:13:00 +0800

    [eva xu][feature] Call introduce method from super class

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,26 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce();
+        introduce += " I am a Student. I am at Class " + klass + ".";
+        return introduce;
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,15 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce();
+        introduce += " I am a Worker. I have a job.";
+        return introduce;
+    }
 }
