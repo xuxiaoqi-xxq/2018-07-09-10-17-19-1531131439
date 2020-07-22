33060ecd5112345b16fcc4b086daf1923f15a891
xxq <xxq_work@163.com>
2020-07-22 20:39:34 +0800

    [eva xu][feature] Add four classes: Klass, Person, Student and Teacher.

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,51 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+
+    protected int number;
+
+    protected Student leader;
+
+    private List<Student> students;
+
+    public Klass(int number) {
+        this.number = number;
+        this.students = new ArrayList();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        boolean isStudentInClass = false;
+        for(Student stu: students){
+            if(stu.getId() == student.getId()){
+                isStudentInClass = true;
+                break;
+            }
+        }
+        if(isStudentInClass){
+            leader = student;
+        } else {
+            System.out.println("It is not one of us.");
+        }
+
+
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
