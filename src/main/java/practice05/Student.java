0f4bc4ccde0b35a944cff429b8e1b511a65797cc
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-22 17:52:36 +0800

    [eva xu][feature] Add person class and it's subclass student and worker

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,31 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+
+    private int klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce();
+        introduce += " I am a Student. I am at Class " + klass + ".";
+        return introduce;
+    }
 }
+
