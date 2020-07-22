d87fbc378ab8f74bcd778f7e20e2c1892acd64ad
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-22 16:22:56 +0800

    [eva xu][feature] Add person class and it's subclass student and teacher.

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Student {
+import practice04.Person;
+
+public class Student extends Person {
+
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce();
+        introduce += " I am a Student. I am at Class " + klass + ".";
+        return introduce;
+    }
 }
+
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,37 @@
 package practice06;
 
-public class Teacher {
+import practice04.Person;
+
+public class Teacher extends Person {
+
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduce = super.introduce();
+        introduce += " I am a Teacher. ";
+        if(klass != null){
+            introduce += "I teach Class " + klass + ".";
+        } else {
+            introduce += "I teach No Class.";
+        }
+        return introduce;
+    }
 }
